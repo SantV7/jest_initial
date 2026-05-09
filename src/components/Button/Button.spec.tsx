80f9697a8773fbc8b588b291611e27c1916095bf
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-05-09 13:19:01 -0300

    Verificando se a o button no render

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -3,7 +3,9 @@ import { render } from "@testing-library/react"
 import Button from "./Button"
 
 describe('<Button>', () => {
-    it('should render button', () => {
-        render(<Button>button</Button>)
+    it('deve renderizar o button', () => {
+        const { getByText} = render(<Button>button</Button>)
+        expect(getByText('button'))
+  
     })
 })
