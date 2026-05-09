b17befd09d2dba01eb2954d851e549ddd68c80cb
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-05-09 13:23:42 -0300

    Agora vamos verificar o teste

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -4,8 +4,9 @@ import Button from "./Button"
 
 describe('<Button>', () => {
     it('deve renderizar o button', () => {
-        const { getByText} = render(<Button>button</Button>)
-        expect(getByText('button'))
+        const { getByText, getByRole} = render(<Button>button</Button>)
+        const buttonElement = expect(getByRole('button')).toBeInTheDocument()
+        const buttonTextElemenet = expect(getByText('button')).toBeInTheDocument()
   
     })
 })
